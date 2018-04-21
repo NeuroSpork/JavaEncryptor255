bffa239f5ac60990536c936ea7696a2a874918c4
ThepersonofDOOM <ThepersonofDOOM@users.noreply.github.com>
2018-04-21 17:19:11 -0400

    ;oium;o
    o;inu9'

--- EncryptedFile.java
+++ EncryptedFile.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package scytale;
 
 /**
- *
- * @author Akuma
  */
-public class EncryptedFile {
-    
+public class EncryptedFile extends ScytaleFile
+{
+    public EncryptedFile(String pathname) {
+        super(pathname);
+        this.setType(true);
+    }
 }
