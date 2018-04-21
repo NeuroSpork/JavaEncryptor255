3495154e14960255d49fd1c98f2c37d8555d0b9f
ThepersonofDOOM <ThepersonofDOOM@users.noreply.github.com>
2018-04-21 17:41:32 -0400

    EncryptionHandlers are handled.

--- DecryptionAlgorithm.java
+++ DecryptionAlgorithm.java
@@ -4,11 +4,22 @@
  * and open the template in the editor.
  */
 package scytale;
-
+import org.jasypt.*;
+import java.io.File.*;
+import java.nio.file.*
 /**
  *
  * @author Akuma
  */
 public class DecryptionAlgorithm {
-    
+    public DecryptionAlgorithm(Byte[] byteK, DecryptedFile decFile, EncryptedFile encFile)
+	{
+		String stringK = new String(byteK);
+		BasicBinaryEncryptor encryptEverything = new BasicBinaryEncryptor();
+		encryptEverything.setPassword(stringK);
+		Path pathA = Paths.get(encFile.getPath());
+		byte[] opData = Files.readAllBytes(PathA);
+		byte[] finData = encryptEverything.decrypt(opData);
+		Files.write(Paths.get(decFile.getPath,finData);
+	}
 }
--- EncryptionAlgorithm.java
+++ EncryptionAlgorithm.java
@@ -4,11 +4,22 @@
  * and open the template in the editor.
  */
 package scytale;
-
+import org.jasypt.*;
+import java.io.File.*;
+import java.nio.file.*
 /**
  *
  * @author Akuma
  */
 public class EncryptionAlgorithm {
-    
+    public EncryptionAlgorithm(Byte[] byteK, DecryptedFile decFile, EncryptedFile encFile)
+	{
+		String stringK = new String(byteK);
+		BasicBinaryEncryptor encryptEverything = new BasicBinaryEncryptor();
+		encryptEverything.setPassword(stringK);
+		Path pathA = Paths.get(decFile.getPath());
+		byte[] opData = Files.readAllBytes(PathA);
+		byte[] finData = encryptEverything.encrypt(opData);
+		Files.write(Paths.get(encFile.getPath,finData);
+	}
 }
